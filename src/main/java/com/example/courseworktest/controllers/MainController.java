19beb7481c050d6ff5fa82ebcc9d25247af094af
Vlad Fo4Ik <zinkovskyi.vladyslav@gmail.com>
2023-10-17 21:14:48 +0200

    add webSocket

--- src/main/java/com/example/courseworktest/controllers/MainController.java
+++ src/main/java/com/example/courseworktest/controllers/MainController.java
@@ -1,17 +1,27 @@
 package com.example.courseworktest.controllers;
 
+import com.example.courseworktest.config.VideoUploadHandler;
+import com.example.courseworktest.core.CompressVideo;
 import com.example.courseworktest.entity.service.ServiceVideo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.socket.WebSocketSession;
 
 @Controller
 public class MainController {
 
     private final ServiceVideo serviceVideo;
+    private final VideoUploadHandler videoUploadHandler;
 
-    public MainController(ServiceVideo serviceVideo) {
+
+    public MainController(ServiceVideo serviceVideo, VideoUploadHandler videoUploadHandler) {
         this.serviceVideo = serviceVideo;
+        this.videoUploadHandler = videoUploadHandler;
     }
 
 
@@ -23,18 +33,22 @@ public class MainController {
         return "index";
     }
 
-    /*@PostMapping("/addVideo")
-    public String addVideo(@RequestParam("video") MultipartFile video, Model model, WebSocketSession session) {
-        try {
-            System.out.println("Video size: " + video.getSize());
-            CompressVideo compressVideo = new CompressVideo();
-            serviceVideo.createVideo(compressVideo.compressVideo(video, session));
-            System.out.println("Video added");
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+    @PostMapping("/addVideo")
+    public String addVideo(@RequestParam("video") MultipartFile video, RedirectAttributes redirectAttributes) {
+        if (video.isEmpty()) {
+            redirectAttributes.addFlashAttribute("name", "Video not selected");
+        } else {
+            try {
+                CompressVideo compressVideo = new CompressVideo(videoUploadHandler);
+
+                serviceVideo.createVideo(compressVideo.compressVideo(video));
+                redirectAttributes.addFlashAttribute("name", "Video " + video.getOriginalFilename() + " added");
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
         }
-        return "index";
-    }*/
+        return "redirect:/";
+    }
 
 
 }
--- src/main/java/com/example/courseworktest/core/CompressVideo.java
+++ src/main/java/com/example/courseworktest/core/CompressVideo.java
@@ -1,5 +1,6 @@
 package com.example.courseworktest.core;
 
+import com.example.courseworktest.config.VideoUploadHandler;
 import net.bramp.ffmpeg.FFmpeg;
 import net.bramp.ffmpeg.FFmpegExecutor;
 import net.bramp.ffmpeg.FFprobe;
@@ -8,79 +9,24 @@ import net.bramp.ffmpeg.job.FFmpegJob;
 import net.bramp.ffmpeg.probe.FFmpegProbeResult;
 import net.bramp.ffmpeg.progress.Progress;
 import net.bramp.ffmpeg.progress.ProgressListener;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
 
-@Component
 public class CompressVideo {
 
+    private final VideoUploadHandler videoUploadHandler;
 
-  /*  public static Path compressVideo(MultipartFile video) {
-        try {
-            FFmpeg ffmpeg = new FFmpeg("C:\\ProgramData\\chocolatey\\lib\\ffmpeg\\tools\\ffmpeg\\bin\\ffmpeg.exe");
-            FFprobe ffprobe = new FFprobe("C:\\ProgramData\\chocolatey\\lib\\ffmpeg\\tools\\ffmpeg\\bin\\ffprobe.exe");
-
-            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-            String outputVideoPath = "output.mp4";
-            String inputVideoPath = video.getOriginalFilename();
-
-            Files.write(Path.of(inputVideoPath), video.getBytes());
-
-            FFmpegProbeResult in = ffprobe.probe(inputVideoPath);
-
-
-            FFmpegBuilder builder = new FFmpegBuilder()
-                    .setInput(in)
-                    .addOutput(outputVideoPath)
-                    .setAudioCodec("aac")
-                    .setVideoCodec("libx265")
-                    .setFormat("mp4")
-                    .setVideoFrameRate(FFmpeg.FPS_24)
-                    .setAudioChannels(FFmpeg.AUDIO_STEREO)
-                    .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL)
-                    .addExtraArgs("-tile-columns", "6")
-                    .done();
-
-
-            FFmpegJob job = executor.createJob(builder, new ProgressListener() {
-
-                // Using the FFmpegProbeResult determine the duration of the input
-                final double duration_ns = in.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
-
-                @Override
-                public void progress(Progress progress) {
-                    double percentage = progress.out_time_ns / duration_ns;
-
-                    // Print out interesting information about the progress
-                    System.out.println(String.format(
-                            "[%.0f%%] status:%s frame:%d time:%s ms fps:%.0f speed:%.2fx",
-                            percentage * 100,
-                            progress.status,
-                            progress.frame,
-                            FFmpegUtils.toTimecode(progress.out_time_ns, TimeUnit.NANOSECONDS),
-                            progress.fps.doubleValue(),
-                            progress.speed
-                    ));
-                }
-            });
-
-            job.run();
-            Files.delete(Path.of(inputVideoPath));
-            return Path.of(outputVideoPath);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public CompressVideo(VideoUploadHandler videoUploadHandler) {
+        this.videoUploadHandler = videoUploadHandler;
     }
-   */
 
-    public Path compressVideo(MultipartFile video, WebSocketSession session) {
+
+    public Path compressVideo(MultipartFile video) {
         try {
             FFmpeg ffmpeg = new FFmpeg("C:\\ProgramData\\chocolatey\\lib\\ffmpeg\\tools\\ffmpeg\\bin\\ffmpeg.exe");
             FFprobe ffprobe = new FFprobe("C:\\ProgramData\\chocolatey\\lib\\ffmpeg\\tools\\ffmpeg\\bin\\ffprobe.exe");
@@ -105,15 +51,17 @@ public class CompressVideo {
                     .addExtraArgs("-tile-columns", "6")
                     .done();
 
-
             FFmpegJob job = executor.createJob(builder, new ProgressListener() {
                 final double duration_ns = in.getFormat().duration * TimeUnit.SECONDS.toNanos(1);
 
                 @Override
                 public void progress(Progress progress) {
-                    double percentage = progress.out_time_ns / duration_ns * 100;
-                    System.out.println("Progress: " + percentage + "%");
-                    // Send progress to frontend
+                    int percentage = (int) (progress.out_time_ns / duration_ns * 100);
+                    try {
+                        videoUploadHandler.sendProgress(percentage);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             });
 
@@ -126,22 +74,4 @@ public class CompressVideo {
             return null;
         }
     }
-
-
-    /*public static void main(String[] args) {
-        //Count time of execution
-        long startTime = System.currentTimeMillis();
-        String inputVideoPath = "nc.mp4"; // Replace with your input video file
-        String outputVideoPath = "nc1.mp4"; // Replace with your output video file
-
-//        compressVideo(inputVideoPath, outputVideoPath);
-
-        // Construct the FFmpeg command
-        long endTime = System.currentTimeMillis();
-        long executionTime = endTime - startTime;
-        System.out.println("Time execution: " + executionTime / 1000 + " seconds");
-
-    }*/
-
-
 }
